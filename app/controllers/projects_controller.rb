aa587d129be89b20855ab1bfebe997e983044762
Brandon Espinoza <espinozab100@gmail.com>
2016-01-09 22:26:51 -0600

    Setup and Styled basic project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :find_project,  only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Awesome Brandon! That project was succesfully saved!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Ahhww Yea!!! Your Article was saved succesfully!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
+
+end
