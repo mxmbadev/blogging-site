084b59cd65162f852d70fde9d948f987ee5f0e64
Brandon Espinoza <espinozab100@gmail.com>
2016-01-12 16:21:37 -0600

    modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: 'https://espinozabrandblog.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
