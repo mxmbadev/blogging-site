90cfcb15bba0e8879f3fdefb1719248c30adac3c
Brandon Espinoza <espinozab100@gmail.com>
2016-01-12 16:53:35 -0600

    added more params to contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,6 +17,6 @@ class ContactsController < ApplicationController
   private
 
   def contacts_params
-    params.require(:contact).permit(:name, :email, :message)
+    params.require(:contact).permit(:name, :email, :message, :nickname)
   end
 end
