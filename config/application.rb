de2de72428519b7a96330bc201f2f9eed2811eb7
Brandon Espinoza <espinozab100@gmail.com>
2016-01-21 12:20:16 -0600

    Debugging config fiels

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Blog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     # For images on production
-    config.serve_static_assets = true
   end
 end
