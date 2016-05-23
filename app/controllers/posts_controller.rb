24a7ac024f9f4c74e46fb2ecdf5bfea200bc2b59
Brandon Espinoza <espinozab100@gmail.com>
2016-05-23 15:02:31 -0500

    blogs only for admin for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post,  only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   def index
     @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
   end
